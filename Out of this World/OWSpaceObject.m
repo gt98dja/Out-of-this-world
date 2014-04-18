b51f871bf0b532ae145954ac1600709a2c8ee973
Sven Jansson <sven@Janssons-iMac.local>
2014-04-18 15:44:24 +0200

    added some variables

--- Out of this World/OWSpaceObject.m
+++ Out of this World/OWSpaceObject.m
@@ -14,7 +14,7 @@
 -(id)init
 {
     self = [self initWithData:nil andImage:nil];
-    retain self;
+    return self;
 }
 
 -(id)initWithData:(NSDictionary *)data andImage:(UIImage *)image
@@ -23,8 +23,17 @@
     
     self.name = data[PLANET_NAME];
     self.gravitationalForce = [data[PLANET_GRAVITY] floatValue];
+    self.diameter = [data[PLANET_DIAMETER] floatValue];
+    self.yearLength = [data[PLANET_YEAR_LENGTH] floatValue];
+    self.dayLength = [data[PLANET_DAY_LENGTH] floatValue];
+    self.temperature = [data[PLANET_TEMPERATURE] floatValue];
+    self.numberOfMoons = [data[PLANET_NUMBER_OF_MOONS] integerValue];
+    self.nickname = data[PLANET_NICKNAME];
+    self.interestFact = data[PLANET_INTERESTING_FACT];
     
+    self.spaceImage = image;
     
+    return self;
 }
 
 @end
